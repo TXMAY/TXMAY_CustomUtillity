4ccce5706e8316e8344a848e22ac1b532987f8e1
TXMAY <juya6388king@naver.com>
2025-01-14 17:06:46 +0900

    First Package Upload

---
+++ Runtime/UserInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomUtillity
+{
+    public class UserInput : SingleTon<UserInput>
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    } 
+}
